4ef59a2521675ea25ad5e1150dc24fa0965c9834
Kostas Tsalikidis <tsalikidhs@gmail.com>
2020-03-03 20:00:34 +0200

    init react app
    Remove example code of a new react app and add routing to the project. A new component "Dashboard".

---
+++ src/components/Dashboard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Dashboard.css';
+
+class Dashboard extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="dashboard">
+        dashboard
+      </div>
+    );
+  }
+}
+export default Dashboard;
