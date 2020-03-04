79f746d2078f307c99e695472a7358791a5e79a7
Kostas Tsalikidis <tsalikidhs@gmail.com>
2020-03-04 09:11:43 +0200

    use https on API
    Rename to apiUrl and use secured http in order to prevent mixed content, as the app will be deployed to now.sh.

--- src/Page.js
+++ src/Page.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const Page = {
-  apiPath: 'http://pagesmanagement.azurewebsites.net/api/',
+  apiUrl: 'https://pagesmanagement.azurewebsites.net/api/',
   getPages() {
     return new Promise(resolve => {
-      axios.get(`${this.apiPath}ResponsivePages`).then(response => {
+      axios.get(`${this.apiUrl}ResponsivePages`).then(response => {
         resolve(response.data); 
       });
     });
